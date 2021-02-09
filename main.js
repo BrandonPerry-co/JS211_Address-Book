deabcc4c726f8460d3cc1c63625f10605b6c840b
brandonPerry-Co <Brandonperrymba@gmail.com>
2021-02-08 22:44:24 -0600

    Changes made

--- main.js
+++ main.js
@@ -17,6 +17,6 @@ async function generateAddress(){
     // } else {
     //     address = data.address
     // }
-    addressText.innerHTML = "Name is: " + data.results[0].name.first + " " + data.results[0].name.last + " | Address is: "  + data.results[0].location.street.number + "  "  + data.results[0].location.street.name + " | Phone Number is: "  + data.results[0].phone + " | Age is: "  + data.results[0].dob.age;
+    addressText.innerHTML = "Name is: " + data.results[0].name.first + " " + data.results[0].name.last + " | Address is: "  + data.results[0].location.street.number + "  "  + data.results[0].location.street.name + " | Phone Number is: "  + data.results[0].phone + " | Age is: "  + data.results[0].dob.age + " | Gender: "  + data.results[0].gender;
     
 }
